6904cd61eb1241cd4cec43b9d0a28f3c767e41a4
Artem <swift.uix@gmail.com>
2021-08-29 13:31:23 +0300

    integrated sound effects

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,18 @@ import React from 'react'
 // COMPONENTS IMPORTS //
 import Navbar from 'Shared/Layout/Navbar'
 import Body from 'Screens/Body'
-import { memoComparison } from 'Shared/Helpers/Functions'
 
 // EXTRA IMPORTS //
+import { memoComparison } from 'Shared/Helpers/Functions'
 
 /////////////////////////////////////////////////////////////////////////////
 
 type PropsType = {}
 
 const App: React.FC<PropsType> = () => {
-  const play = () => {}
-
   return (
     <>
       <Navbar />
-      <button onClick={play}>ss</button>
       <Body />
     </>
   )
